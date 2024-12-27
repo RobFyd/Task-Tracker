93f82bdbae50659fc615931bff269ae1e4af26c2
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-12-27 23:32:39 +0000

    Enhance Button component to accept onClick prop and integrate into Form component

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,5 +1,5 @@
 import styles from "./Button.module.css";
 
-export function Button({ children }) {
-  return <button>{children}</button>;
+export function Button({ children, onClick }) {
+  return <button onClick={onClick}>{children}</button>;
 }
--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,10 +1,17 @@
+import { Button } from "../Button/Button";
 import styles from "./Form.module.css";
 
 export function Form() {
   return (
     <form className={styles.form}>
       <input type="text" className={styles.input} />
-      <button className={styles.button}>Add Task</button>
+      <Button
+        onClick={() => {
+          alert("Task added!");
+        }}
+      >
+        Add Task
+      </Button>
     </form>
   );
 }
