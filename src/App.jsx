f6c600408524a1e0f88fd80144647f52a86ff002
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-12-27 22:55:53 +0000

    Enhance App component layout and add global styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import "./App.css";
 
 function App() {
-  return <div>Task Tracker</div>;
+  return (
+    <div>
+      <header>
+        <h1>Task Tracker</h1>
+        <div>5 tasks</div>
+        <button>&nbsp;+&nbsp;</button>
+      </header>
+    </div>
+  );
 }
 
 export default App;
