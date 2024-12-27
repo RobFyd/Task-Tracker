208cf72772f9b31573cf3f600ee83920084237f7
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-12-27 23:13:42 +0000

    Update App component layout with header styling and button enhancements

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import styles from "./App.module.css";
 function App() {
   return (
     <div className={styles.container}>
-      <header>
-        <h1>Task Tracker</h1>
-        <div>5 tasks</div>
-        <button>&nbsp;+&nbsp;</button>
+      <header className={styles.header}>
+        <div>
+          <h1>Task Tracker</h1>
+          <h2>5 tasks</h2>
+        </div>
+        <button className={styles.button}>+</button>
       </header>
     </div>
   );
