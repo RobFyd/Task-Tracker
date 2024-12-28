e0a2aef3301cabd6370bd24874c66973f0fa6d8c
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-12-28 21:55:55 +0000

    Enhance TodoItem component with styling and button updates

--- src/components/TodoItem/TodoItem.jsx
+++ src/components/TodoItem/TodoItem.jsx
@@ -1,9 +1,12 @@
+import { Button } from "../Button/Button";
+import styles from "./TodoItem.module.css";
+
 export function TodoItem({ name, done }) {
   return (
-    <li>
-      <span>{name}</span>
-      {!done && <button>Done</button>}
-      <button>Delete</button>
+    <li className={styles.item}>
+      <span className={styles.name}>{name}</span>
+      {!done && <Button>Done</Button>}
+      <Button>Remove</Button>
     </li>
   );
 }
